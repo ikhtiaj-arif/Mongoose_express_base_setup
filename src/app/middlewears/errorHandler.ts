cb048e293e02c4661ec7db7d55eb728a3e5f8f44
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2024-12-06 23:02:38 +0600

    uncaughtexception and unhandledrejection handles

--- src/app/middlewears/errorHandler.ts
+++ src/app/middlewears/errorHandler.ts
@@ -9,11 +9,12 @@ import handleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidationErrors';
 import handleCastError from '../errors/handleCastError';
 import handleDuplicateError from '../errors/handleDuplicateError';
+import AppError from '../errors/AppError';
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   //setting default values
-  let statusCode = error.statusCode || 500;
-  let message = error.message || 'Something went wrong';
+  let statusCode = 500;
+  let message = 'Something went wrong';
 
   let errorSources: TErrorSources = [
     { path: '', message: 'Something went wrong!' },
@@ -33,18 +34,35 @@ const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
-  }else if(error?.name === 'CastError'){
+  } else if (error?.name === 'CastError') {
     const simplifiedError = handleCastError(error);
 
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
-  }else if(error?.code === 11000){
+  } else if (error?.code === 11000) {
     const simplifiedError = handleDuplicateError(error);
 
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
+  } else if (error instanceof AppError) {
+    statusCode = error?.statusCode;
+    message = error?.message;
+    errorSources = [
+      {
+        path: '',
+        message: error.message,
+      },
+    ];
+  } else if (error instanceof Error) {
+    message = error?.message;
+    errorSources = [
+      {
+        path: '',
+        message: error.message,
+      },
+    ];
   }
 
   res.status(statusCode).json({
